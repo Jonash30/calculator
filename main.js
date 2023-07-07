30b8156d802b9db5972c04b8a7c20e4da93a3807
Jonash Gania <jonashtiglao@gmail.com>
2023-07-07 15:52:39 +0800

    Fix the Handling and Displaying

--- main.js
+++ main.js
@@ -1,35 +1,151 @@
-let num1 = 4;
-let num2 = 4;
-let operator;
+// const display = document.getElementById('display');
+// const output = document.querySelector('.output')
+// const buttons = document.querySelectorAll("button");
 
-function add(a,b) {
-    return a + b;
+// buttons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const buttonValue = button.dataset.value;
+//     const currentValue = display.value;
+//     const isOperator = button.classList.contains('operator');
+//     const endsWithOperator = /[\+\-\*\/]$/.test(currentValue);
+
+//     if (buttonValue === '=') {
+//       const expression = currentValue.split(' ');
+//       const num1 = parseFloat(expression[0]);
+//       const operator = expression[1];
+//       const num2 = parseFloat(expression[2]);
+
+//       if (!isNaN(num1) && !isNaN(num2) && operator) {
+//         const result = operate(num1, num2, operator);
+//         display.value = result !== null ? result : '';
+//         output.textContent = display.value;
+//       }
+//     } else {
+//         if (isOperator && endsWithOperator) {
+//           display.value = currentValue.slice(0, -1);
+//         }
+//         display.value += isOperator ? ` ${buttonValue} ` : buttonValue;
+//       }
+//   });
+// });
+
+
+
+
+
+const buttons = document.querySelectorAll("button");
+
+
+const calculator = {
+  displayOutput: '',
+  displayValue: '0',
+  firstOperand: null,
+  secondOperand: false,
+  operator: null,
 }
-console.log(add(num1,num2));
 
-function subtract(a,b) {
-    return a - b;
+function updateDisplay(){
+  const output = document.querySelector('.output')
+  const display = document.getElementById('display');
+  display.value = calculator.displayValue;
 }
-console.log(subtract(num1,num2));
+updateDisplay();
 
-function multiply(a,b) {
-    return a * b;
+function inputDigit(digit){
+  const { displayValue, secondOperand } = calculator;
+  if(secondOperand === true){
+    calculator.displayValue = digit;
+    calculator.secondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+  
+  console.log(calculator);
 }
-console.log(multiply(num1,num2));
 
-function divide(a,b) {
-    return a / b;
+
+function inputDecimal(dot){
+  if(!calculator.displayValue.includes(dot)){
+    calculator.displayValue += dot;
+  }
 }
-console.log(divide(num1, num2));
 
-function operate(num1,num2,operator){
-    if(operator === '+'){
-        add();
-    } else if(operator === '-'){
-        subtract();
-    } else if(operator === '*'){
-        multiply();
-    } else if(operator === '/'){
-        divide();
+function handleOperator(nextOperator){
+  const { firstOperand, displayValue, operator} = calculator;
+  const inputValue = parseFloat(displayValue);
+
+  if(firstOperand === null && !isNaN(inputValue)){
+    calculator.firstOperand = inputValue;
+  } else if(operator){
+    const result = operate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+
+  calculator.secondOperand = true;
+  calculator.operator = nextOperator;
+
+  console.log(calculator);
+}
+
+function operate(firstOperand, secondOperand, operator) {
+  if (operator === '+') {
+    return add(firstOperand, secondOperand);
+  } else if (operator === '-') {
+    return subtract(firstOperand, secondOperand);
+  } else if (operator === '×') {
+    return multiply(firstOperand, secondOperand);
+  } else if (operator === '÷') {
+    return divide(firstOperand, secondOperand);
+  }
+
+  return secondOperand;
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', (event) =>{
+    const { target } = event;
+    
+    if(!target.matches('button')){
+      return
     }
+    if(target.classList.contains('operator')){
+      handleOperator(target.value);
+      updateDisplay();
+      return;
+    }
+    if(target.classList.contains('all-clear')){
+      console.log('all-clear', target.value)
+      return;
+    }
+    if(target.classList.contains('clear')){
+      console.log('clear', target.value)
+      return;
+    }
+    if(target.classList.contains('decimal')){
+      inputDecimal(target.value)
+      updateDisplay();
+      return
+    }
+    inputDigit(target.value);
+    updateDisplay();
+  })
+})
+
+function add(a, b) {
+  return a + b;
 }
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
